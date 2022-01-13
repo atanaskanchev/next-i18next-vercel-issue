a2850cb2af5fa8af7402180ad5a94fd3101c89ec
Atanas Kanchev <atanas.kanchev@valens-services.co.uk>
2022-01-13 11:26:47 +0000

    public moved to root

--- apps/next-i18next-vercel-issue/next-i18next.config.js
+++ apps/next-i18next-vercel-issue/next-i18next.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
 
-const isRunningInBrowser = typeof window !== undefined;
+// const isRunningInBrowser = typeof window !== undefined;
+
 const isRunningOnLocalhost =
   !!process.env['NEXT_PUBLIC_IS_RUNNING_ON_LOCALHOST'];
+
 const isRunningOnVercel = !!process.env.VERCEL;
 
 console.log({
-  isRunningInBrowser,
+  // isRunningInBrowser,
   isRunningOnLocalhost,
   isRunningOnVercel,
 });
@@ -15,14 +17,11 @@ module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
-
-    ...(isRunningOnLocalhost && {
-      localePath: path.resolve(
-        './apps/next-i18next-vercel-issue/public/public/locales'
-      ),
-    }),
-    ...(isRunningOnVercel && {
-      localePath: path.resolve('./public/locales'),
-    }),
   },
+  ...(isRunningOnLocalhost && {
+    localePath: path.resolve('./public/locales'),
+  }),
+  ...(isRunningOnVercel && {
+    localePath: path.resolve('./public/locales'),
+  }),
 };
